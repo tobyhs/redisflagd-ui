7ebae2876d8a5522c4839e780b0d85c963b6161a
Toby Hsieh <thrasher13b@gmail.com>
2025-10-31 17:41:47 -0700

    Proxy /api/ requests to backend in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/': 'http://localhost:9292',
+    }
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['vitest-setup.ts'],
