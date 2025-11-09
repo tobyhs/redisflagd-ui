4a576dc5d13cb401b71729a82bf839bfcc91ebed
Toby Hsieh <thrasher13b@gmail.com>
2025-11-08 20:52:31 -0800

    Change renderRoute to use BrowserRouter
    MemoryRouter doesn't allow me to check window.location in tests.

--- frontend/tests/rendering.tsx
+++ frontend/tests/rendering.tsx
@@ -1,15 +1,16 @@
 import { render } from '@testing-library/react'
-import { MemoryRouter } from 'react-router'
+import { BrowserRouter } from 'react-router'
 
 import { AppRoutes } from '../src/AppRoutes'
 import { TestProvider } from './TestProvider'
 
 export function renderRoute(path: string) {
+  window.history.replaceState(null, '', path)
   return render(
     <TestProvider>
-      <MemoryRouter initialEntries={[path]}>
+      <BrowserRouter>
         <AppRoutes />
-      </MemoryRouter>
+      </BrowserRouter>
     </TestProvider>,
   )
 }
