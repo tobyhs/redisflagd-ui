de6a6895d93e694b897506cd850a88a0be8682ba
Toby Hsieh <thrasher13b@gmail.com>
2025-11-12 14:04:42 -0800

    Add `@mantine/notifications` library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router'
 
--- frontend/src/AppLayout.tsx
+++ frontend/src/AppLayout.tsx
@@ -1,9 +1,11 @@
+import { Notifications } from '@mantine/notifications'
 import { Outlet } from 'react-router'
 
 export function AppLayout() {
   return (
     <>
       <h1>RedisFlagd UI</h1>
+      <Notifications />
       <Outlet />
     </>
   )
